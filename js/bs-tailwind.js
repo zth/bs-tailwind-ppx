ff480eeccad69fc594cc93cdccb1cbaa3e89d503
Gabriel Nordeborn <gabbe.nord@gmail.com>
2019-11-13 09:27:26 +0100

    Update script to use utils

--- js/bs-tailwind.js
+++ js/bs-tailwind.js
@@ -3,22 +3,19 @@ let p = require("postcss");
 let fs = require("fs");
 let tailwind = require("tailwindcss");
 let path = require("path");
+let { processCss, extractNodeData } = require("./utils");
 
 let src = fs.readFileSync(process.argv[2]);
 
 let rules = [];
 
-p([tailwind()])
-  .process(src, {
-    from: "a.css",
-    to: "a.out.css"
-  })
-  .then(res => {
-    res.root.nodes.forEach(node => {
-      if (node.type === "rule" && node.selector.startsWith(".")) {
-        rules.push(node.selector.slice(1).replace(/\\/g, ""));
-      }
-    });
-
-    fs.writeFileSync("./bs-tailwind.json", JSON.stringify(rules));
+processCss(src).then(res => {
+  res.root.nodes.forEach(node => {
+    let nodeData = extractNodeData(node);
+    if (nodeData) {
+      rules.push(nodeData);
+    }
   });
+
+  fs.writeFileSync("./bs-tailwind.json", JSON.stringify(rules));
+});
